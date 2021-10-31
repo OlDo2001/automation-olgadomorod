dd6985aa6a7659844a08f3d2c9088b59697dc044
Olga Domorod <olga.domorod@gmail.com>
2021-10-31 22:57:25 +0100

    Create, change and delete a bill

--- frontend-tests/cypress/integration/regression-test-suite.spec.js
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -70,7 +70,38 @@ describe('Regression test suite',function(){
         cy.contains('Tester Hotel Overview')
         //cy.get('.user > .btn').click()
         //cy.contains('Login')
-
     })
+    it('Create, change and delete a bill',function(){
+        cy.visit('http://localhost:3000')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Welcome tester01!')
+        cy.get(':nth-child(3) > .btn').click()
+        cy.contains('Bills')
+        cy.get('h2 > .btn').click()
+        cy.contains('New Bill')
+        //Create new bill
+        cy.get('input').type('2000')
+        cy.get('.blue').click()
+        //Confirm that the bill was created
+        cy.get('.bills > :nth-child(2)').should('contain', 'ID: 2')
+        //Change the bill
+        cy.get(':nth-child(2) > .action > img').click()
+        cy.get('.menu > :nth-child(1)').click()
+        cy.get('.checkbox').click()
+        cy.get('.blue').click()
+        //Confirm that the bill was changed 
+        cy.get(':nth-child(2) > .paid').should('contain', 'Yes')
+        //Delete the bill
+        cy.get(':nth-child(2) > .action > img').click()
+        cy.get('.menu > :nth-child(2)').click()
+        //Confirm that the bill was deleted
+        cy.get('.card').should('not.contain', 'ID: 2')
+        //Logout
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
+    })
+
 
 })
